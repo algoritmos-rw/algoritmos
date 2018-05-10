19b6836eb6c9d4c44f444911f78c2778f30265e9
Federico del Mazo <federicodelmazo@hotmail.com>
2018-05-10 12:38:17 -0300

    Fechas de tps lindo

--- assets/js/publicacionTPs.js
+++ assets/js/publicacionTPs.js
@@ -1,12 +1,21 @@
 ---
 ---
 
+var monthNames = [
+    "Enero", "Febrero", "Marzo",
+    "Abril", "Mayo", "Junio", "Julio",
+    "Agosto", "Septiembre", "Octubre",
+    "Noviembre", "Diciembre"
+  ];
+
+
 window.onload = agregarTPsATabla;
 function agregarTPsATabla() {
     var dtHoy = new Date();
-    var hoy = (dtHoy.getYear() + "/" + dtHoy.getMonth() + 1) + "/" + (dtHoy.getDate());
+    var hoy = (dtHoy.getYear() + "/" + dtHoy.getMonth() + 1) + "/" + (dtHoy.getDate()+1);
     {% for x in site.data.trabajos %}{% assign tp = x[1] %}
         var dtPublicacion = new Date("{{tp.publicacion}}");
+        var dtEntrega = new Date("{{tp.entrega}}");
         var publicacion = (dtPublicacion.getYear() + "/" + dtPublicacion.getMonth() + 1) + "/" + (dtPublicacion.getDate()+1);
         
         var tabla = document.getElementById("tabla-trabajos");
@@ -17,9 +26,11 @@ function agregarTPsATabla() {
         
         if (hoy >= publicacion) fila.innerHTML += "<td> <a href='{{tp.zip_link}}'>{{tp.zip}}</a> </td>"
         else fila.innerHTML += "<td> {{tp.zip}} </td>"
-        
-        fila.innerHTML += "<td> {{tp.publicacion}} </td>" 
-        fila.innerHTML += "<td> {{tp.entrega}} </td>" 
+        var prettyPublicacion = dtPublicacion.getDate() + ' de ' + monthNames[dtPublicacion.getMonth()];
+        var prettyEntrega = dtEntrega.getDate() + ' de ' + monthNames[dtEntrega.getMonth()];
+
+        fila.innerHTML += "<td>" + prettyPublicacion + "</td>" 
+        fila.innerHTML += "<td>" + prettyEntrega + "</td>" 
         tabla.appendChild(fila)
     {% endfor %}
 
