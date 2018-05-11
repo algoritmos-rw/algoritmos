3d936f03cde1ed33a2211323be261499be7bf9b4
Federico del Mazo <federicodelmazo@hotmail.com>
2018-05-11 15:13:57 -0300

    Comparo fechas por Date object

--- assets/js/publicacionTPs.js
+++ assets/js/publicacionTPs.js
@@ -11,32 +11,28 @@ var monthNames = [
 
 window.onload = agregarTPsATabla;
 function agregarTPsATabla() {
-    var dtHoy = new Date();
-    var hoy = (dtHoy.getYear() + "/" + dtHoy.getMonth() + 1) + "/" + (dtHoy.getDate()+1);
-    
+    var dtHoy = new Date();    
     var dtFinCuatrimestre = new Date("{{site.data.cuatrimestre.fin_cuatrimestre}}");
-    var finCuatrimestre = (dtFinCuatrimestre.getYear() + "/" + dtFinCuatrimestre.getMonth() + 1) + "/" + (dtFinCuatrimestre.getDate()+1);
     
     {% for x in site.data.trabajos %}{% assign tp = x[1] %}
         var dtPublicacion = new Date("{{tp.publicacion}}");
         var dtEntrega = new Date("{{tp.entrega}}");
-        var publicacion = dtPublicacion.getYear() + "/" + (dtPublicacion.getMonth() + 1) + "/" + (dtPublicacion.getDate()+1);
         
         var tabla = document.getElementById("tabla-trabajos");
         var fila = document.createElement("tr")
         
-        if (hoy >= publicacion && hoy <= finCuatrimestre) fila.innerHTML += "<td> <a href={{tp.enunciado_link}}'>{{tp.id}}</a> </td>"
+        if (dtHoy >= dtPublicacion && dtHoy <= dtFinCuatrimestre) fila.innerHTML += "<td> <a href={{tp.enunciado_link}}'>{{tp.id}}</a> </td>"
         else fila.innerHTML += "<td> {{tp.id}} </td>"
         
-        if (hoy >= publicacion && hoy <= finCuatrimestre) fila.innerHTML += "<td> <a href='{{tp.zip_link}}'>{{tp.zip}}</a> </td>"
+        if (dtHoy >= dtPublicacion && dtHoy <= dtFinCuatrimestre) fila.innerHTML += "<td> <a href='{{tp.zip_link}}'>{{tp.zip}}</a> </td>"
         else fila.innerHTML += "<td> {{tp.zip}} </td>"
         var prettyPublicacion = dtPublicacion.getDate()+1 + ' de ' + monthNames[dtPublicacion.getMonth()];
         var prettyEntrega = dtEntrega.getDate()+1 + ' de ' + monthNames[dtEntrega.getMonth()];
 
-        if (hoy <= finCuatrimestre) fila.innerHTML += "<td>" + prettyPublicacion + "</td>"
+        if (dtHoy <= dtFinCuatrimestre) fila.innerHTML += "<td>" + prettyPublicacion + "</td>"
         else fila.innerHTML += "<td> </td>"
 
-        if (hoy <= finCuatrimestre) fila.innerHTML += "<td>" + prettyEntrega + "</td>"
+        if (dtHoy <= dtFinCuatrimestre) fila.innerHTML += "<td>" + prettyEntrega + "</td>"
         else fila.innerHTML += "<td> </td>"
 
         tabla.appendChild(fila)
