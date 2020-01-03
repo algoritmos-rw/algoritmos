dce16d2c4fed288a2678bf06cd7b4f2a4c7cb9e8
Martin Buchwald <martin.buchwald@mulesoft.com>
2020-01-03 10:33:11 -0300

    inicio enunciado

---
+++ static/tps/2020_1/tp2/mensajes.h
@@ -0,0 +1,17 @@
+#ifndef __MENSAJES__H_
+#define __MENSAJES__H_
+
+#define PACIENTE_ENCOLADO "Paciente %s encolado\n"
+#define CANT_PACIENTES_ENCOLADOS "%zu paciente(s) en espera para %s\n"
+#define ENOENT_PACIENTE "ERROR: no existe el paciente '%s'\n"
+#define ENOENT_ESPECIALIDAD "ERROR: no existe la especialidad '%s'\n"
+#define ENOENT_URGENCIA "Error: grado de urgencia no identificado ('%s')\n"
+
+#define PACIENTE_ATENDIDO "Se atiende a %s\n"
+#define SIN_PACIENTES "No hay pacientes en espera"
+#define ENOENT_DOCTOR "ERROR: no existe el doctor '%s'\n"
+
+#define DOCTORES_SISTEMA "%zu doctor(es) en el sistema\n"
+#define INFORME_DOCTOR "%zu: %s, especialidad %s, %zu paciente(s) atendido(s)"
+
+#endif
